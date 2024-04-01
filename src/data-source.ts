16ab89c00a82a2aff1af575e144d32f86092365d
abhishekbhattarai0 <abhishekbhattarai831@gmail.com>
2024-04-01 10:30:22 +0545

    news

---
+++ src/data-source.ts
@@ -0,0 +1,24 @@
+import "reflect-metadata"
+import { DataSource } from "typeorm"
+import { User } from "./entity/User"
+import { employ } from "./entity/employ"
+import { student } from "./entity/student"
+import { Teacher } from "./entity/Teacher"
+import { UserProfile } from "./entity/UserProfile"
+import { Login } from "./entity/Login"
+  // type: "mysql",
+    // host: "localhost",
+    // port: 3306,
+    // username: "root",
+    // password: "Ihba62813",
+    // database: "newdb",
+export const AppDataSource = new DataSource({
+    type:'postgres',
+    url:"postgres://default:OcQg86XBeIhW@ep-bitter-hat-a47pcfew-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require",
+    // url:"postgres://default:OcQg86XBeIhW@ep-bitter-hat-a47pcfew-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require",
+    synchronize: false,
+    logging: false,
+    entities: [User, employ, student, Teacher, UserProfile, Login],
+    migrations: [],
+    subscribers: [],
+})
